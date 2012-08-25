351508fa07d79de273cbd9bce3b239de81ef256f
Vincent Fan <vincent.fxz@gmail.com>
2012-08-25 20:51:13 +0800

    IE8 fix

--- src/main/webapp/js/index3.js
+++ src/main/webapp/js/index3.js
@@ -1,120 +1,245 @@
+require(["dojox/data/JsonRestStore", "dojo/store/Memory", "dojo/store/Cache", "dojox/grid/EnhancedGrid", "dojo/data/ObjectStore", "dojo/query", "dojo/ready", "dijit/form/TextBox", "dijit/form/Button", "dijit/Menu", "dijit/MenuItem", "dijit/form/ComboButton", "dojo/dom", "dojo/_base/xhr", "dojo/i18n!/dojo-release-1.7.2/dojox/grid/enhanced/nls/zh/Filter.js", "dojox/grid/enhanced/plugins/Search", "dojox/grid/enhanced/plugins/Filter", "dojo/parser", "dijit/layout/TabContainer", "dijit/layout/ContentPane", "dijit/form/DropDownButton", "dijit/TooltipDialog", "dijit/form/TextBox", "dojox/grid/enhanced/plugins/IndirectSelection", "dojox/grid/enhanced/plugins/Printer", "dojo/domReady!"], function(JsonRestStore, Memory, Cache, EnhancedGrid, ObjectStore, query, ready, TextBox, Button, Menu, MenuItem, ComboButton, dom, xhr, i18n) {
 
-require([
-    "dojox/data/JsonRestStore",
-    "dojo/store/Memory",
-    "dojo/store/Cache",
-    "dojox/grid/EnhancedGrid",
-    "dojo/data/ObjectStore",
-    "dojo/query",
-    "dojo/ready",
-    "dijit/form/TextBox",
-    "dijit/form/Button",
-    "dijit/Menu", 
-    "dijit/MenuItem", 
-    "dijit/form/ComboButton",
-    "dojo/dom",
-    "dojo/_base/xhr",
-    "dojo/i18n!/dojo-release-1.7.2/dojox/grid/enhanced/nls/zh/Filter.js",
-    "dojox/grid/enhanced/plugins/Search",
-    "dojox/grid/enhanced/plugins/Filter",
-    "dojo/parser",
-    "dijit/layout/TabContainer", 
-    "dijit/layout/ContentPane",
-    "dijit/form/DropDownButton",
-    "dijit/TooltipDialog",
-    "dijit/form/TextBox",
-    "dojox/grid/enhanced/plugins/IndirectSelection",
-    "dojox/grid/enhanced/plugins/Printer",
-    "dojo/domReady!"
-], function(JsonRestStore, Memory, Cache, EnhancedGrid, ObjectStore, 
-    query, ready, TextBox, Button, Menu, MenuItem, ComboButton,
-    dom,xhr,i18n){  
-    
-    ready(function(){
-        ammeterRecordStore = new JsonRestStore({target: "/ammeter_record/list "});
+    ready(function() {
+
+        console.log(dojo.style("main_container","width"));
+
+        var ammeter_cell_width = dojo.style("main_container","width") * 0.18;
+
+        ammeterRecordStore = new JsonRestStore({
+            target: "/ammeter_record/list "
+        });
         ammeterRecordGrid = new EnhancedGrid({
-            store: recordDataStore =ammeterRecordStore,
-            structure: [
-                        {name: "电表记录编号", field: "id", width: "25%", canSort: true},
-                        {name: "电表名称", field: "ammeterName", width: "25%", canSort: true},
-                        {name: "电表数值", field: "ammeterValue", width: "25%", canSort: true},
-                        {name: "电表记录日期", field: "recordDate", width: "25%", canSort: true},
-                        ],
-            plugins:{
+            autoHeight : 15,
+            store: recordDataStore = ammeterRecordStore,
+            structure: [{
+                name: "电表记录编号",
+                field: "id",
+                width: "25%",
+                canSort: true
+            }, {
+                name: "电表名称",
+                field: "ammeterName",
+                width: "25%",
+                canSort: true
+            }, {
+                name: "电表数值",
+                field: "ammeterValue",
+                width: "25%",
+                canSort: true
+            }, {
+                name: "电表记录日期",
+                field: "recordDate",
+                width: "25%",
+                canSort: true
+            }, ],
+            plugins: {
                 search: true,
-                filter:true,
+                filter: true,
                 printer: true,
-                indirectSelection: {headerSelector:true, width:"40px", styles:"text-align: center;"}
-             }
-        },"ammeter_record_grid");
+                indirectSelection: {
+                    headerSelector: true,
+                    width: "40px",
+                    styles: "text-align: center;"
+                }
+            }
+        }, "ammeter_record_grid");
         ammeterRecordGrid.startup();
-        
-        ammeterStore = new JsonRestStore({target:"/ammeter/list/"});
+
+        ammeterStore = new JsonRestStore({
+            target: "/ammeter/list/"
+        });
         ammeterGrid = new EnhancedGrid({
             store: dataStore = ammeterStore,
-            structure: [
-                         { name: "电表编号", field: "id", width: "25%",canSort:true},
-                         { name: "电表名称", field: "name", width: "25%",editable:true},
-                         { name: "泵名称", field: "pumpName", width: "25%",editable:true},
-                         { name:"项目名称", field: "projectName",width: "25%",editable:true},
-                         { name:" 公司名称", field: "companyName",width: "25%",editable:true}
-                    ],
-             plugins:{
+            autoWidth : true,
+            structure: [{
+                name: "电表编号",
+                field: "id",
+                width: ammeter_cell_width  * 0.5 + "px",
+                canSort: true
+            }, {
+                name: "电表名称",
+                field: "name",
+                width: ammeter_cell_width + "px",
+                editable: true
+            }, {
+                name: "泵名称",
+                field: "pumpName",
+                width: ammeter_cell_width + "px",
+                editable: true
+            }, {
+                name: "项目名称",
+                field: "projectName",
+                width: ammeter_cell_width + "px",
+                editable: true
+            }, {
+                name: " 公司名称",
+                field: "companyName",
+                width: ammeter_cell_width + "px",
+                editable: true
+            }],
+            plugins: {
                 search: true,
-                filter:true,
+                filter: true,
                 printer: true,
-                indirectSelection: {headerSelector:true, width:"40px", styles:"text-align: center;"}
-             }
-        }, "ammeter_grid"); 
-        ammeterGrid.startup();  
-        
+                indirectSelection: {
+                    headerSelector: true,
+                    width: "40px",
+                    styles: "text-align: center;"
+                }
+            }
+        }, "ammeter_grid");
+        ammeterGrid.startup();
+
         //construt add button
-        var add_ammeter_btn=new Button({
-            label:"新建",
-            onClick:function(){
-                var form_content={
-                    name:dom.byId("name").value,
-                    companyName:dom.byId("companyName").value,
-                    projectName:dom.byId("projectName").value,
-                    pumpName:dom.byId("pumpName").value,
+        var add_ammeter_btn = new Button({
+            label: "新建",
+            onClick: function() {
+                var form_content = {
+                    name: dom.byId("name").value,
+                    companyName: dom.byId("companyName").value,
+                    projectName: dom.byId("projectName").value,
+                    pumpName: dom.byId("pumpName").value,
                 };
                 xhr.post({
-                    form: "add_ammeter_form", // read the url: from the action="" of the <form>
-                    timeout: 3000, // give up after 3 seconds
-                    content:form_content,
-                    handleAs:"json",
-                    load:function(new_ammeter){
-                            dataStore.newItem(new_ammeter);
-                        }
+                    form: "add_ammeter_form",
+                    // read the url: from the action="" of the <form>
+                    timeout: 3000,
+                    // give up after 3 seconds
+                    content: form_content,
+                    handleAs: "json",
+                    load: function(new_ammeter) {
+                        dataStore.newItem(new_ammeter);
+                        dataStore.save();
+                    }
                 });
             }
-        },"add_ammeter_btn");
-        add_ammeter_btn.startup();      
+        }, "add_ammeter_btn");
+        add_ammeter_btn.startup();
 
         //construt save button
-        var save_button=new Button({
-            label:"保存",
-            onClick: function(){
+        var save_button = new Button({
+            label: "保存",
+            onClick: function() {
                 console.log("sava clicked");
                 dataStore.save();
-                
+
             }
-        },"save_button");
+        }, "save_button");
         save_button.startup();
-        
+
         //construt delete button
-        var delete_button=new Button({
-            label:"删除",
-            onClick: function(){
-                var ammeters_selected=ammeterGrid.selection.getSelected();
-                if(ammeters_selected.length){
-                    for(key in ammeters_selected){
+        var delete_button = new Button({
+            label: "删除",
+            onClick: function() {
+                console.log("delete");
+                var ammeters_selected = ammeterGrid.selection.getSelected();
+                if (ammeters_selected.length) {
+                    for (key in ammeters_selected) {
+                        console.log(ammeters_selected[key]);
                         dataStore.deleteItem(ammeters_selected[key]);
+                        dataStore.save();
                     }
                 }
             }
-        },"delete_button");
+        }, "delete_button");
         delete_button.startup();
-        
+
+        var construtUserPane = function construtUserPane() {
+                userStore = new JsonRestStore({
+                    target: "/user/list/"
+                });
+                userGrid = new EnhancedGrid({
+                    store: userDataStore = userStore,
+                    structure: [{
+                        name: "用户编号",
+                        field: "id",
+                        height: "24px",
+                        width: "25%",
+                        canSort: true
+                    }, {
+                        name: "用户名称",
+                        field: "name",
+                        width: "25%",
+                        editable: true
+                    },  {
+                        name: "项目名称",
+                        field: "projectName",
+                        width: "25%",
+                        editable: true
+                    }, {
+                        name: "公司名称",
+                        field: "companyName",
+                        width: "25%",
+                        editable: true
+                    }],
+                    plugins: {
+                        search: true,
+                        filter: true,
+                        printer: true,
+                        indirectSelection: {
+                            headerSelector: true,
+                            width: "40px",
+                            styles: "text-align: center;"
+                        }
+                    }
+                }, "user_grid");
+                userGrid.startup();
+
+                //construt add button
+                var add_user_btn = new Button({
+                    label: "新建",
+                    onClick: function() {
+                        var form_content = {
+                            name: dom.byId("username").value,
+                            companyName: dom.byId("userCompanyName").value,
+                            projectName: dom.byId("userProjectName").value,
+                        };
+                        xhr.post({
+                            form: "add_user_form",
+                            // read the url: from the action="" of the <form>
+                            timeout: 3000,
+                            // give up after 3 seconds
+                            content: form_content,
+                            handleAs: "json",
+                            load: function(new_user) {
+                                userDataStoreuserDataStore.newItem(new_user);
+                                userDataStore.save();
+                            }
+                        });
+                    }
+                }, "add_user_btn");
+                add_user_btn.startup();
+
+                //construt save button
+                var save_button = new Button({
+                    label: "保存",
+                    onClick: function() {
+                        userDataStore.save();
+
+                    }
+                }, "user_save_button");
+                save_button.startup();
+
+                //construt delete button
+                var delete_button = new Button({
+                    label: "删除",
+                    onClick: function() {
+                        var ammeters_selected = ammeterGrid.selection.getSelected();
+                        if (ammeters_selected.length) {
+                            for (key in ammeters_selected) {
+                                console.log(ammeters_selected[key]);
+                                userDataStore.deleteItem(ammeters_selected[key]);
+                                userDataStore.save();
+                            }
+                        }
+                    }
+                }, "user_delete_button");
+                delete_button.startup();
+
+            }
+
+         construtUserPane();   
+
     });
 });
