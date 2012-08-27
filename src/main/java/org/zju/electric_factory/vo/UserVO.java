a74c5d96269d7e1d7d95d953fd481d9bc303e433
Vincent Fan <vincent.fxz@gmail.com>
2012-08-27 17:13:46 +0800

    user VO

---
+++ src/main/java/org/zju/electric_factory/vo/UserVO.java
@@ -0,0 +1,33 @@
+package org.zju.electric_factory.vo;
+
+public class UserVO {
+
+	private Long id;
+	private String username;
+	private String email;
+	private String roles;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+}
