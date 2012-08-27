5ff853562072b04251696b4fef4320605ef23b5b
Vincent Fan <vincent.fxz@gmail.com>
2012-08-27 17:12:59 +0800

    change

--- src/main/java/org/zju/electric_factory/controller/UserController.java
+++ src/main/java/org/zju/electric_factory/controller/UserController.java
@@ -1,6 +1,9 @@
 package org.zju.electric_factory.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.zju.electric_factory.entity.Role;
 import org.zju.electric_factory.entity.User;
 import org.zju.electric_factory.service.UserManager;
+import org.zju.electric_factory.vo.UserVO;
 
 @Controller
 @RequestMapping("/user")
@@ -22,19 +27,41 @@ public class UserController {
 	private UserManager userManager;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/list", headers = "Accept=application/json")
-	public @ResponseBody List<User> getUsers() {
-		return userManager.getAllUsers();
+	public @ResponseBody List<UserVO> getUsers() {
+		List<User> users = userManager.getAllUsers();
+		List<UserVO> userVOs = new ArrayList<UserVO>();
+		for(User user : users){
+			UserVO userVo	= new UserVO();
+			userVo.setUsername(user.getUsername());
+			userVo.setId(user.getId());
+			userVo.setEmail(user.getEmail());
+			StringBuffer sb = new StringBuffer();
+			for(Role role : user.getRoles()){
+				sb.append(role.getName());
+				sb.append(" ");
+			}
+			userVo.setRoles(sb.toString());
+			userVOs.add(userVo);
+			
+		}
+		return userVOs;
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT,value="/list/{id}",headers="Accept=application/json")
+	@RequestMapping(method=RequestMethod.PUT,value="/list/{id}",headers ="Accept=application/json")
     public @ResponseBody User updateUser(@PathVariable String id,@RequestBody User user){
         userManager.updateUser(user);
         return user;
     }
 	
-	@RequestMapping(method=RequestMethod.GET,value="/list/{id}",headers="Accept=application/json")
-    public @ResponseBody User getUser(@PathVariable String id,@RequestBody User user){
+	@RequestMapping(method=RequestMethod.GET, value="/list/{id}", headers="Accept=application/json")
+    public @ResponseBody User getUser(@PathVariable String id){
 		return userManager.getUser(Long.parseLong(id));
+    }
+	
+	@RequestMapping(method=RequestMethod.POST,value="/add",headers="Accept=application/json")
+    public @ResponseBody User addUser(User user){
+		userManager.createUser(user);
+		return user;
     }
 	
 	
