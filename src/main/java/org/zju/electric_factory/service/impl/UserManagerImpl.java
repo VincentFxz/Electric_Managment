2e344f6ae2858bc798779493c7fd63cec1e9d598
Vincent Fan <vincent.fxz@gmail.com>
2012-08-27 17:09:01 +0800

    fix get by name bug

--- src/main/java/org/zju/electric_factory/service/impl/UserManagerImpl.java
+++ src/main/java/org/zju/electric_factory/service/impl/UserManagerImpl.java
@@ -4,47 +4,72 @@ import java.util.List;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.zju.electric_factory.dao.UserDAO;
 import org.zju.electric_factory.entity.User;
 import org.zju.electric_factory.service.UserManager;
 
+@Component
+@Transactional
 public class UserManagerImpl implements UserManager {
-    
-    @Autowired
-    private UserDAO userDAO; 
 
-    public User getCurrentUser() {
-        final Long currentUserId = (Long) SecurityUtils.getSubject().getPrincipal();
-        if( currentUserId != null ) {
-            return getUser(currentUserId);
-        } else {
-            return null;
-        }
-    }
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserManagerImpl.class);
+	@Autowired
+	private UserDAO userDAO;
 
-    public void createUser(String username, String email, String password) {
-        User user = new User();
-        user.setUsername(username);
-        user.setEmail(email);
-        user.setPassword( new Sha256Hash(password).toHex() );
-        userDAO.createUser( user );
-    }
+	public User getCurrentUser() {
+		final Long currentUserId = (Long) SecurityUtils.getSubject()
+				.getPrincipal();
+		if (currentUserId != null) {
+			return getUser(currentUserId);
+		} else {
+			return null;
+		}
+	}
 
-    public List<User> getAllUsers() {
-        return userDAO.getAllUsers();
-    }
+	public void createUser(String username, String email, String password) {
+		User user = new User();
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setPassword(new Sha256Hash(password).toHex());
+		userDAO.createUser(user);
+	}
 
-    public User getUser(Long userId) {
-        return userDAO.getUser(userId);
-    }
+	public void createUser(User user) {
+		
+		user.setPassword(new Sha256Hash(user.getPassword()).toHex());
+		logger.info("create user : name '" + user.getUsername()
+				+ "', password '" + user.getPassword() + "', Email '"
+				+ user.getEmail() + "'");
+		userDAO.createUser(user);
+	}
 
-    public void deleteUser(Long userId) {
-        userDAO.deleteUser( userId );
-    }
+	public List<User> getAllUsers() {
+		return userDAO.getAllUsers();
+	}
 
-    public void updateUser(User user) {
-        userDAO.updateUser( user );
-    }
+	public User getUser(Long userId) {
+		return userDAO.getUser(userId);
+	}
+
+	public void deleteUser(Long userId) {
+		userDAO.deleteUser(userId);
+	}
+
+	public void updateUser(User user) {
+		userDAO.updateUser(user);
+	}
+
+	@Override
+	public User getUserByName(String name) {
+		return userDAO.findUser(name);
+	}
+	
+	
 
 }
