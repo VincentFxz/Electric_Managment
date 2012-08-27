56fca7fb76a35691180137c54658f7e4b85eb0eb
Vincent Fan <vincent.fxz@gmail.com>
2012-08-27 17:14:10 +0800

    change

--- src/main/java/org/zju/electric_factory/dao/impl/UserDAOImpl.java
+++ src/main/java/org/zju/electric_factory/dao/impl/UserDAOImpl.java
@@ -2,12 +2,18 @@ package org.zju.electric_factory.dao.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.zju.electric_factory.dao.UserDAO;
 import org.zju.electric_factory.entity.User;
+import org.zju.electric_factory.service.impl.UserManagerImpl;
 
 @Repository
 public class UserDAOImpl extends HibernateDAO<User, Long> implements UserDAO{
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserDAOImpl.class);
 
 	@Override
 	public User getUser(Long userId) {
@@ -16,7 +22,7 @@ public class UserDAOImpl extends HibernateDAO<User, Long> implements UserDAO{
 
 	@Override
 	public User findUser(String username) {
-		return super.findUniqueBy(username,username);
+		return super.findUniqueBy("username",username);
 	}
 
 	@Override
