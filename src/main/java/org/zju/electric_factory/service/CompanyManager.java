2312dc60e1a7cdc4d505476e8e921e954a91c254
Vincent Fan <vincent.fxz@gmail.com>
2012-09-09 15:21:48 +0800

    commit company manager

---
+++ src/main/java/org/zju/electric_factory/service/CompanyManager.java
@@ -0,0 +1,18 @@
+package org.zju.electric_factory.service;
+
+import java.util.List;
+
+import org.zju.electric_factory.entity.Company;
+
+public interface CompanyManager {
+	
+	public Company getCompanyByUserId(Long id);
+	public List<Company> getCompanies();
+	public List<Company> getCompaniesAscById();
+	public List<Company> getCompaniesDescById();
+	public List<Company> getCompaniesAscByName();
+	public List<Company> getCompanesDescByName();
+	public void deleteCompanyById(String id);
+	public void editCompany(Company company);
+	public void add(Company company);
+}
---
+++ src/main/java/org/zju/electric_factory/service/impl/CompanyManagerImpl.java
@@ -0,0 +1,79 @@
+package org.zju.electric_factory.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.zju.electric_factory.dao.CompanyDAO;
+import org.zju.electric_factory.dao.UserCompanyLinkDAO;
+import org.zju.electric_factory.entity.Company;
+import org.zju.electric_factory.entity.UserCompanyLink;
+import org.zju.electric_factory.service.CompanyManager;
+
+@Component
+public class CompanyManagerImpl implements CompanyManager {
+
+	@Autowired
+	private UserCompanyLinkDAO userCompanyLinkDAO;
+	@Autowired
+	private CompanyDAO companyDAO;
+	
+	@Override
+	public Company getCompanyByUserId(Long id) {
+		Company company = null;
+		List<UserCompanyLink> companyUserLink = userCompanyLinkDAO.getByUserId(id);
+		if(null != companyUserLink){
+			if(0 < companyUserLink.size()){
+				Long companyId = companyUserLink.get(0).getCompanyId();
+				company = companyDAO.getCompanyById(companyId);
+			}
+		}
+		return company;
+	}
+
+	@Override
+	public List<Company> getCompanies() {
+		return companyDAO.getAllCompanies();
+	}
+
+	@Override
+	public List<Company> getCompaniesAscById() {
+		return companyDAO.getAllByOrder("id", true);
+	}
+
+	@Override
+	public List<Company> getCompaniesDescById() {
+		return companyDAO.getAllByOrder("id", false);
+	}
+
+	@Override
+	public List<Company> getCompaniesAscByName() {
+		return companyDAO.getAllByOrder("companyName", true);
+	}
+
+	@Override
+	public List<Company> getCompanesDescByName() {
+		return companyDAO.getAllByOrder("companyName", false);
+	}
+
+	@Override
+	public void deleteCompanyById(String id) {
+		if(null != id){
+			Long companyId = Long.parseLong(id);
+				companyDAO.delete(companyId);
+		}
+		
+	}
+
+	@Override
+	public void editCompany(Company company) {
+		companyDAO.update(company);
+		
+	}
+
+	@Override
+	public void add(Company company) {
+		companyDAO.add(company);
+	}
+	
+}
