31c264b9240ffb9d5899361d0466661afa690e2a
Vincent Fan <vincent.fxz@gmail.com>
2012-11-22 15:21:10 +0800

    add project VO

---
+++ src/main/java/org/zju/electric_factory/vo/ProjectVO.java
@@ -0,0 +1,57 @@
+package org.zju.electric_factory.vo;
+
+import java.util.Date;
+
+
+public class ProjectVO {
+
+    private Long id;
+    
+    private String projectName;
+    
+    private String projectDescription;
+    
+    private Date startDate;
+    
+    private Date endDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
