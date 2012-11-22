291c62403a64af3a4ba20e93911d0fb5c0b5c300
Vincent Fan <vincent.fxz@gmail.com>
2012-11-22 15:18:14 +0800

    add get ammeterrecord by ammeter id

--- src/main/java/org/zju/electric_factory/controller/AmmeterRecordController.java
+++ src/main/java/org/zju/electric_factory/controller/AmmeterRecordController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +20,13 @@ public class AmmeterRecordController {
     @Autowired
     private AmmeterRecordManager ammeterRecordManager;
     
-    @RequestMapping(method=RequestMethod.GET,value="/list",headers="Accept=application/json")
+    @RequestMapping(method=RequestMethod.GET,value="/list/{id}",headers="Accept=application/json")
+    public  @ResponseBody List<AmmeterRecord> listAmmeterRecordsByAmmeterId(@PathVariable String id){
+    	if(null == id) return null;
+        return ammeterRecordManager.getByAmmeter(Long.parseLong(id));
+    }
+    
+    @RequestMapping(method=RequestMethod.GET,value="/list/",headers="Accept=application/json")
     public  @ResponseBody List<AmmeterRecord> listAmmeterRecords(){
     	
         return ammeterRecordManager.getAll();
